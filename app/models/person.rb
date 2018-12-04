97e89d6d95bfaaeafff8177fc139e9332354c939
Bing Chou <virtuallybing@gmail.com>
2018-12-04 05:33:13 -0700

    Add mentors and mentees scopes

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,7 @@
 class Person < ApplicationRecord
   has_many :preferences
   has_many :preferred_people, through: :preferences, source: 'preferred_person'
+
+  scope :mentors, -> { where(is_mentor: true).order(created_at: :asc) }
+  scope :mentees, -> { where(is_mentee: true).order(created_at: :asc) }
 end
